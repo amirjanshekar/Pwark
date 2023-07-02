660337cd7e8b0f848d3961ad6d00da03a3e1af95
amir janshekar <64432039+amirjanshekar@users.noreply.github.com>
2023-07-02 05:35:34 -0400

    end

--- Utils/exporter.py
+++ Utils/exporter.py
@@ -5,16 +5,25 @@ import openpyxl
 
 
 def exporter(data, work_type, min_month='', max_month='', year=''):
-
     with open(f'export_{work_type}_{year}_{min_month} to {max_month}.csv', 'w', encoding="utf-8-sig",
               newline='') as out:
+        cells = []
+        for j in range(4, 14):
+            for i in ['E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O']:
+                cells.append(f'{i}{j}')
         csv_out = csv.writer(out)
         csv_out.writerow(['row', 'year', 'month', 'day', 'product', 'type', 'work', 'produce', 'amount'])
         for item in data:
             srcfile = openpyxl.load_workbook('./Forms/{}.xlsx'.format(work_type), read_only=False, keep_vba=True)
             sheet_name = srcfile.get_sheet_by_name('1')
             sheet_name['E2'] = item[4]
-            sheet_name['K2'] = f"{item[1]}/{item[2]}/{item[3]}"
+            for m in cells:
+                if sheet_name[f'{i}{j}'].value is None:
+                    sheet_name[f'{m}'] = item[8]
+                    break
+                else:
+                    break
+
             srcfile.save('./Output/{}.xlsm'.format(item[4]))
             csv_out.writerow(item)
 
